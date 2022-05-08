0a9790467dde34d138d64971d3542702a2e3e020
aleks4ay <aleks4ay@gmail.com>
2022-05-08 18:59:50 +0300

    Added 3 panels and Board klick listener

--- js/lines.js
+++ js/lines.js
@@ -1,13 +1,70 @@
 function init() {
    var field = document.getElementById("field");
-   var fieldImg = document.getElementById("fieldImg");
+   var cellSize = 64;
+   var offsetCells = 313;
+   let mainImage = new PanelImage("fieldImg", true);
+   let boardImage = new PanelImage("cellsImg", true);
+   let resultImage = new PanelImage("resultImg", false);
+   let newWinnerImage = new PanelImage("newWinnerImg", false);
 
-   var leftImgOffset = window.outerWidth/2 - fieldImg.naturalWidth/2 -field.offsetLeft; //window.outerWidth/2 -600 -field.offsetLeft;
-   fieldImg.style.left = leftImgOffset;
+   var zeroX = window.outerWidth/2 - mainImage.img.naturalWidth/2 + offsetCells - field.offsetLeft;
+   var zeroY = boardImage.img.offsetTop + 80;
+   resize();
 
    window.onresize = function(e) {
-     let newLeftImgOffset = window.outerWidth/2 - fieldImg.naturalWidth/2 -field.offsetLeft;
-     fieldImg.style.left = newLeftImgOffset;
-     console.log("w1 = " + w1 + " px;");
+      resize();
+   };
+
+   
+   boardImage.img.onclick = function(e) {
+      var x = (e.pageX - zeroX - field.offsetLeft) / cellSize | 0;
+      var y = (e.pageY - zeroY)  / cellSize | 0;
+      console.log("["+e.pageX+":"+e.pageY+"], {"+x+":"+y+"}");
+   };
+
+
+   function resize() {
+     zeroX =  window.outerWidth/2 - mainImage.img.naturalWidth/2 - field.offsetLeft + offsetCells;
+     mainImage.setLeft(zeroX - offsetCells);
+     boardImage.setLeft(zeroX);
+     resultImage.setLeft(zeroX + cellSize);
+     newWinnerImage.setLeft(zeroX + cellSize);
+     console.log("zeroX = " + zeroX);
+     console.log("zeroY = " + zeroY);
+   };
+}
+
+
+
+
+class PanelImage {
+   id = "id";
+   width = 0;
+   height = 0;
+   left = 0;
+   top = 0;
+   visible = true;
+
+   constructor(id, visible) {
+      this.id = id;
+      this.img = document.getElementById(id);
+      this.width = this.img.naturalWidth;
+      this.height = this.img.naturalHeight;
+      this.left = this.img.offsetLeft;
+      this.top = this.img.offsetTop;
+      this.visible = visible;
+      if (!visible) {
+         this.img.style.display = "none";
+      }
+   }
+
+   printConsoleWidth() {
+      console.log(this.width);
+   }
+
+   setLeft(x) {
+      this.left = x;
+      this.img.style.left = x;
    }
 }
+
