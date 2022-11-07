1198049bfd703ddd423af5ff6b851dff11d45585
Sealiv <sai.79.ua@gmail.com>
2022-11-07 13:19:35 +0200

    Deleted class Cell. Structure more simple now

--- js/lines.js
+++ js/lines.js
@@ -1,12 +1,14 @@
+var score = 0;
 var zeroX = 0;
 var zeroY = 0;
+var maxColor = 7;
 var cellSize = 64;
 var offsetCells = 313;
 var selected = null;
 var ballId = 0;
 var balls = [];
 var cells = [];
-var emptyCells = [];
+var emptyCount = 9 * 9;
 
 function init() {
    var field = document.getElementById("field");
@@ -27,17 +29,11 @@ function init() {
       if (selected == null) return;
       var x = (e.pageX - zeroX - field.offsetLeft) / cellSize | 0;
       var y = (e.pageY - zeroY)  / cellSize | 0;
-      if (cells[x][y].ball == null && checkRoad(selected.cell, cells[x][y])) {
+      if (cells[x][y] == null && checkRoad(new EmptyCell(x, y))) {
          selected.setPosition(x, y);
-         checkTheSameColorAndDelete(cells[x][y]);
+         checkTheSameColorAndDelete(selected);
          selected.doJamp();
       }
-   };
-
-   boardImage.img.ondblclick = function(e) {
-      var x = (e.pageX - zeroX - field.offsetLeft) / cellSize | 0;
-      var y = (e.pageY - zeroY)  / cellSize | 0;
-      new Ball(x, y, getRandom(6) + 1);
    };
 
 
@@ -47,8 +43,8 @@ function init() {
      boardImage.setLeft(zeroX);
      resultImage.setLeft(zeroX + cellSize);
      newWinnerImage.setLeft(zeroX + cellSize);
-     console.log("zeroX = " + zeroX);
-     console.log("zeroY = " + zeroY);
+   //   console.log("zeroX = " + zeroX);
+   //   console.log("zeroY = " + zeroY);
      fillSell();
    };
 }
@@ -80,64 +76,46 @@ class PanelImage {
    }
 }
 
-class Cell {
-   constructor(x, y, ball) {
+class EmptyCell {
+   constructor(x, y) {
       this.x = x;
       this.y = y;
-      this.ball = ball;
    }
-
-   setBall(newBall) {
-      this.ball = newBall;
-   }
-
-   deleteBall() {
-      this.ball = null;
-   }
-
-   toString(){
-      let id = this.ball != null ? this.ball.id : "-";
-      return "{" + this.x + ", " + this.y + ", " + id + "}" ;
-   }
+   toString() {return "EmptyCell{" + this.x + ", " + this.y + "}";}
 }
 
 class Ball {
    id = "";
    color = 1;
-   cell = null;
 
    constructor(x, y, color) {
+      this.x = x;
+      this.y = y;
       this.color = color;
       this.img = document.createElement("img");
       this.id = "b_" + ++ballId;
       this.img.setAttribute("id", this.id);
       this.img.setAttribute("src", "img/png/s" + color + ".png");
-      this.img.classList.add("color" + color);
       document.getElementById("field").append(this.img);
       this.setPosition(x, y);
-      checkTheSameColorAndDelete(cells[x][y]);
+      checkTheSameColorAndDelete(this);
       this.img.onclick = clickOnBall;
       balls.push(this);
    }
 
    setPosition(x, y) {
       this.setImageOffsets(this.img, x, y);
-      this.setCell(cells[x][y]);
+      if(selected !== null) {
+         cells[selected.x][selected.y] = null;
+      }
+      cells[x][y]=this;
+      this.x = x;
+      this.y = y;
    }
 
    setImageOffsets(image, x, y) {
       image.style.left = zeroX + 7 + x * cellSize | 0;
       image.style.top = zeroY - 11 - cellSize + y * cellSize | 0;
-   }
-
-   setCell(newCell){
-      if (this.cell != null) {
-         emptyCells.push(this.cell);
-         this.cell.ball = null;
-      }
-      this.cell = newCell;
-      newCell.ball = this;
-      emptyCells.splice(emptyCells.indexOf(newCell), 1);
    }
 
    doJamp() {
@@ -148,8 +126,8 @@ class Ball {
    toString(){
       return "Ball["
       + "id: " + this.id
+      + ", {" + this.x + ", " + this.y + "}"
       + ", color: " + this.color
-      + ", cell={" + this.cell.x + ", " + this.cell.y + "}"
       + ", img: " + this.img.getAttribute("src")
       + "]";
    }
@@ -157,13 +135,28 @@ class Ball {
 
 function addBalls() {
    for (let i=0; i<3; i++) {
-      if(emptyCells.length === 0) {
+      if(emptyCount === 0) {
          console.log("THE END");
          return;
       }
-      let position = getRandom(emptyCells.length - 1);
-      let newCell = emptyCells[position];
-      let newBall = new Ball(newCell.x, newCell.y, getRandom(0) + 1);
+      let newCell = findPosition();
+      let newBall = new Ball(newCell.x, newCell.y, getRandom(maxColor-1) + 1);
+   }
+}
+function findPosition() {
+   let position = getRandom(emptyCount - 1);
+   emptyCount--;
+   let index = 0;
+   for(let x=0; x<9; x++) {
+      for(let y=0; y<9; y++) {
+         if(cells[x][y] === null) {
+            if(index === position){
+               return new EmptyCell(x, y);
+            } else {
+               index++;
+            }
+         }
+      }
    }
 }
 
@@ -207,19 +200,17 @@ function fillSell() {
    for (let x = 0; x < 9; x++) {
       var cellsX = [];
       for (let y = 0; y < 9; y++) {
-         var newCell = new Cell(x, y, null);
-         cellsX.push(newCell);
-         emptyCells.push(newCell);      
+         cellsX.push(null);
       }
       cells.push(cellsX);  
    }
 }
 
 
-function checkRoad(cellStart, cellEnd) {
+function checkRoad(cellEnd) {
    var checkedCells = [];
    var needCheck = [];
-   needCheck.push(cellStart);
+   needCheck.push(new EmptyCell(selected.x, selected.y));
    return checkRoadArray(cellEnd, checkedCells, needCheck);
 }
 
@@ -228,36 +219,38 @@ function checkRoadArray(cellEnd, arrChecked, arrNeedCheck) {
       console.log("Road doesn't exist...")
       return false;
    }
-   let tempCell = arrNeedCheck.pop();
+   let tempCell = arrNeedCheck.shift();
    if (Math.abs(tempCell.x - cellEnd.x) + Math.abs(tempCell.y - cellEnd.y) === 1) {
       return true;
    }
    let x = tempCell.x;
    let y = tempCell.y;
-   if (tempCell.x > 0 && checkOneCell(cells[x - 1][y], arrChecked)) {
-      arrNeedCheck.push(cells[x - 1][y]);
+   if (tempCell.x > 0 && isNeedAddCellToCheck(x - 1, y, arrChecked, arrNeedCheck)) {
+      arrNeedCheck.push(new EmptyCell(x - 1, + y));
    }
-   if (tempCell.x < 8 && checkOneCell(cells[x + 1][y], arrChecked)) {
-      arrNeedCheck.push(cells[x + 1][y]);
+   if (tempCell.x < 8 && isNeedAddCellToCheck(x + 1, y, arrChecked, arrNeedCheck)) {
+      arrNeedCheck.push(new EmptyCell(x + 1, y));
    }
-   if (tempCell.y > 0 && checkOneCell(cells[x][y - 1], arrChecked)) {
-      arrNeedCheck.push(cells[x][y - 1]);
+   if (tempCell.y > 0 && isNeedAddCellToCheck(x, y - 1, arrChecked, arrNeedCheck)) {
+      arrNeedCheck.push(new EmptyCell(x, y - 1));
    }
-   if (tempCell.y < 8 && checkOneCell(cells[x][y + 1], arrChecked)) {
-      arrNeedCheck.push(cells[x][y + 1]);
+   if (tempCell.y < 8 && isNeedAddCellToCheck(x, y + 1, arrChecked, arrNeedCheck)) {
+      arrNeedCheck.push(new EmptyCell(x, y + 1));
    }
    arrChecked.push(tempCell);
    return checkRoadArray(cellEnd, arrChecked, arrNeedCheck);
 }
 
-function checkOneCell(cell, arrChecked) {
-   if(arrChecked.indexOf(cell) >= 0) {
+function isNeedAddCellToCheck(x, y, alreadyCheckedArray, needCheckArray){
+   if(cells[x][y] !== null) {
       return false;
    }
-   return cell.ball === null;
+   let checkedElement = alreadyCheckedArray.find((i)=> i.x === x & i.y === y);
+   let alreadyAddedElement = needCheckArray.find((i)=> i.x === x & i.y === y);
+   return !checkedElement && !alreadyAddedElement;
 }
 
-function checkTheSameColorAndDelete(cell) {
+function checkTheSameColorAndDelete(ball) {
    let left = 0;
    let right = 0;
    let up = 0;
@@ -266,104 +259,103 @@ function checkTheSameColorAndDelete(cell) {
    let rightUp = 0;
    let leftDown = 0;
    let rightDown = 0;
-   let x = cell.x;
-   let y = cell.y;
+   let x = ball.x;
+   let y = ball.y;
 
    for(let i = x-1; i>=0; i--) {
-      if (cells[i][y].ball !== null && cells[i][y].ball.color === cell.ball.color) {
+      if (cells[i][y] !== null && cells[i][y].color === ball.color) {
          left++;
       } else break;
    }
    for(let i = x+1; i<=8; i++) {
-      if (cells[i][y].ball !== null && cells[i][y].ball.color === cell.ball.color) {
+      if (cells[i][y] !== null && cells[i][y].color === ball.color) {
          right++;
       } else break;
    }
    for(let i = y-1; i>=0; i--) {
-      if (cells[x][i].ball !== null && cells[x][i].ball.color === cell.ball.color) {
+      if (cells[x][i] !== null && cells[x][i].color === ball.color) {
          up++;
       } else break;
    }
    for(let i = y+1; i<=8; i++) {
-      if (cells[x][i].ball !== null && cells[x][i].ball.color === cell.ball.color) {
+      if (cells[x][i] !== null && cells[x][i].color === ball.color) {
          down++;
       } else break;
    }
 
    for(let i = 1; x-i>=0 & y-i>=0; i++) {
-      if (cells[x-i][y-i].ball !== null && cells[x-i][y-i].ball.color === cell.ball.color) {
+      if (cells[x-i][y-i] !== null && cells[x-i][y-i].color === ball.color) {
          leftUp++;
       } else break;
    }
    for(let i = 1; x+i<9 & y+i<9; i++) {
-      if (cells[x+i][y+i].ball !== null && cells[x+i][y+i].ball.color === cell.ball.color) {
+      if (cells[x+i][y+i] !== null && cells[x+i][y+i].color === ball.color) {
          rightDown++;
       } else break;
    }
    for(let i = 1; x-i>=0 & y+i<9; i++) {
-      if (cells[x-i][y+i].ball !== null && cells[x-i][y+i].ball.color === cell.ball.color) {
+      if (cells[x-i][y+i] !== null && cells[x-i][y+i].color === ball.color) {
          leftDown++;
       } else break;
    }
    for(let i = 1; x+i<9 & y-i>=0; i++) {
-      if (cells[x+i][y-i].ball !== null && (cells[x+i][y-i].ball.color === cell.ball.color)) {
+      if (cells[x+i][y-i] !== null && (cells[x+i][y-i].color === ball.color)) {
          rightUp++;
       } else break;
    }
+
    let arrayToDelete = [];
    if(up + down >= 4) {
       for(let i=y-up; i<=y+down; i++){
-         if(i !== y) {
-            arrayToDelete.push(cells[x][i].ball);
-         }
+         arrayToDelete.push(cells[x][i]);
       }
    }
    if(left + right >= 4) {
       for(let i=x-left; i<=x+right; i++){
-         if (i !== x) {
-            arrayToDelete.push(cells[i][y].ball);
-         }
+         arrayToDelete.push(cells[i][y]);
       }
    }
    if(leftDown + rightUp >= 4) {
       let length = leftDown + rightUp;
       for(let i=0; i<= length; i++){
-         if(i !== leftDown) {
-            arrayToDelete.push(cells[x-leftDown+i][y+leftDown-i].ball);
-         }
+         arrayToDelete.push(cells[x-leftDown+i][y+leftDown-i]);
       }
    }
    if(leftUp + rightDown >= 4) {
       let length = leftUp + rightDown;
-      console.log("rightDown=" + rightDown + ", leftUp=" + leftUp);
       for(let i=0; i<= length; i++){
-         if(i !== leftUp) {
-            console.log("{" + (x-leftUp+i) + "," + y-leftUp+i + "}");
-            arrayToDelete.push(cells[x-leftUp+i][y-leftUp+i].ball);
-         }
+         arrayToDelete.push(cells[x-leftUp+i][y-leftUp+i]);
       }
-      console.log("arr: " + arrayToDelete);
    }
    
    if(arrayToDelete.length > 0) {
-      arrayToDelete.push(cells[x][y].ball);
+      deleteBalls(getBallsWithoutDuplicates(arrayToDelete));
    }
-
-   deleteBalls(arrayToDelete);
 }
 
 function deleteBalls(ballsToDelete) {
-   console.log("removing... " + ballsToDelete);
    while(ballsToDelete.length > 0) {
       let ball = ballsToDelete.pop();
       remove(ball);
+      score = score + 2;
    }
+   console.log("Score = " + score);
 }
 
 function remove(ball) {
-   ball.cell.deleteBall();
-   emptyCells.push(ball.cell);
+   cells[ball.x][ball.y] = null;
    document.getElementById(ball.id).remove();
    balls.splice(balls.indexOf(ball), 1);
    ball = null;
+}
+
+function getBallsWithoutDuplicates(arr) {
+   let distinctArray = [];
+   while(arr.length > 0) {
+      let currentElement = arr.shift();
+      if(!(distinctArray.find((i)=> i === currentElement))) {
+         distinctArray.push(currentElement);
+      }
+   }
+   return distinctArray;
 }
